4dc25d67eee3a7b6027f491b083fba646e2b0c58
Plezo <Plezo@protonmail.com>
2022-12-06 19:57:48 -0500

    Initial Commit

---
+++ Fitkeeping/FitkeepingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitkeepingApp.swift
+//  Fitkeeping
+//
+//  Created by Patryk Lezon on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct FitkeepingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
